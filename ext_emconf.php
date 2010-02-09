82b1c5208211e3ea4b720482d837c6a0216ffc41
kraftb <kraftb@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-02-09 13:56:59 +0000

    Uploaded extension to TER
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/kb_nescefe/trunk@29860 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,13 +1,13 @@
 <?php
 
 ########################################################################
-# Extension Manager/Repository config file for ext: "kb_nescefe"
+# Extension Manager/Repository config file for ext "kb_nescefe".
 #
-# Auto generated 02-12-2009 16:02
+# Auto generated 09-02-2010 14:56
 #
 # Manual updates:
-# Only the data in the array - anything else is removed by next write.
-# "version" and "dependencies" must not be touched!
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
 ########################################################################
 
 $EM_CONF[$_EXTKEY] = array(
@@ -40,7 +40,9 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:31:{s:9:"ChangeLog";s:4:"1d8e";s:10:"README.txt";s:4:"9228";s:37:"class.tx_kbnescefe_contentPreview.php";s:4:"79ce";s:37:"class.tx_kbnescefe_dbNewContentEl.php";s:4:"3360";s:27:"class.tx_kbnescefe_func.php";s:4:"3012";s:31:"class.tx_kbnescefe_itemproc.php";s:4:"6a7f";s:35:"class.tx_kbnescefe_t3libtcemain.php";s:4:"afcf";s:28:"class.ux_t3lib_clipboard.php";s:4:"328e";s:26:"class.ux_tx_cms_layout.php";s:4:"8041";s:21:"ext_conf_template.txt";s:4:"8cb2";s:12:"ext_icon.gif";s:4:"54da";s:17:"ext_localconf.php";s:4:"be75";s:14:"ext_tables.php";s:4:"8048";s:14:"ext_tables.sql";s:4:"0b55";s:32:"icon_tx_kbnescefe_containers.gif";s:4:"401f";s:13:"locallang.xml";s:4:"b0b6";s:16:"locallang_db.xml";s:4:"0d13";s:15:"nested_icon.gif";s:4:"0f7a";s:16:"nested_icon2.gif";s:4:"ea6d";s:7:"tca.php";s:4:"436a";s:30:"pi1/class.tx_kbnescefe_pi1.php";s:4:"0302";s:28:"pi1/ext_typoscript_setup.txt";s:4:"8fbd";s:17:"pi1/locallang.xml";s:4:"dfeb";s:14:"doc/manual.sxw";s:4:"bce1";s:16:"static/setup.txt";s:4:"904c";s:14:"res/2cols.html";s:4:"a575";s:14:"res/3cols.html";s:4:"d1b3";s:17:"res/be_2cols.html";s:4:"a575";s:13:"res/be_3.html";s:4:"d1b3";s:22:"res/be_horiz_cols.html";s:4:"978e";s:19:"res/horiz_cols.html";s:4:"fa62";}',
+	'_md5_values_when_last_written' => 'a:31:{s:9:"ChangeLog";s:4:"c07b";s:10:"README.txt";s:4:"9228";s:37:"class.tx_kbnescefe_contentPreview.php";s:4:"004c";s:37:"class.tx_kbnescefe_dbNewContentEl.php";s:4:"e3fa";s:27:"class.tx_kbnescefe_func.php";s:4:"7212";s:31:"class.tx_kbnescefe_itemproc.php";s:4:"f29b";s:29:"class.tx_kbnescefe_layout.php";s:4:"e138";s:35:"class.tx_kbnescefe_t3libtcemain.php";s:4:"9cbf";s:28:"class.ux_t3lib_clipboard.php";s:4:"3be9";s:26:"class.ux_tx_cms_layout.php";s:4:"107a";s:21:"ext_conf_template.txt";s:4:"8cb2";s:12:"ext_icon.gif";s:4:"54da";s:17:"ext_localconf.php";s:4:"866b";s:14:"ext_tables.php";s:4:"56b4";s:14:"ext_tables.sql";s:4:"5158";s:32:"icon_tx_kbnescefe_containers.gif";s:4:"401f";s:13:"locallang.xml";s:4:"b0b6";s:16:"locallang_db.xml";s:4:"6649";s:15:"nested_icon.gif";s:4:"0f7a";s:16:"nested_icon2.gif";s:4:"ea6d";s:7:"tca.php";s:4:"436a";s:14:"doc/manual.sxw";s:4:"d93a";s:30:"pi1/class.tx_kbnescefe_pi1.php";s:4:"416d";s:17:"pi1/locallang.xml";s:4:"dfeb";s:14:"res/2cols.html";s:4:"a575";s:14:"res/3cols.html";s:4:"d1b3";s:17:"res/be_2cols.html";s:4:"a575";s:17:"res/be_3cols.html";s:4:"d1b3";s:22:"res/be_horiz_cols.html";s:4:"978e";s:19:"res/horiz_cols.html";s:4:"fa62";s:16:"static/setup.txt";s:4:"0689";}',
+	'suggests' => array(
+	),
 );
 
 ?>
