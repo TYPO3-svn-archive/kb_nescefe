4dbfe9fd85eb17ed4c114b2b5ee1179e16396566
kraftb <kraftb@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-07-23 08:57:50 +0000

    * Changed a "width" style for special 4.4.x CSS * Uploaded Extensio to TER as 1.1.1 * Updated documentation
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/kb_nescefe/trunk@35969 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "kb_nescefe".
 #
-# Auto generated 10-05-2010 16:21
+# Auto generated 23-07-2010 10:24
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'Allows to created nested content elements.',
 	'category' => 'plugin',
 	'shy' => 0,
-	'version' => '1.0.3',
+	'version' => '1.1.0',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
@@ -40,7 +40,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:31:{s:9:"ChangeLog";s:4:"b30a";s:10:"README.txt";s:4:"6572";s:20:"class.ext_update.php";s:4:"2439";s:37:"class.tx_kbnescefe_contentPreview.php";s:4:"f53d";s:37:"class.tx_kbnescefe_dbNewContentEl.php";s:4:"e3fa";s:27:"class.tx_kbnescefe_func.php";s:4:"7212";s:31:"class.tx_kbnescefe_itemproc.php";s:4:"f29b";s:29:"class.tx_kbnescefe_layout.php";s:4:"e138";s:35:"class.tx_kbnescefe_t3libtcemain.php";s:4:"357f";s:28:"class.ux_t3lib_clipboard.php";s:4:"3be9";s:26:"class.ux_tx_cms_layout.php";s:4:"107a";s:21:"ext_conf_template.txt";s:4:"bdb2";s:12:"ext_icon.gif";s:4:"54da";s:17:"ext_localconf.php";s:4:"ca85";s:14:"ext_tables.php";s:4:"3966";s:14:"ext_tables.sql";s:4:"5158";s:32:"icon_tx_kbnescefe_containers.gif";s:4:"401f";s:13:"locallang.xml";s:4:"a182";s:16:"locallang_db.xml";s:4:"6649";s:15:"nested_icon.gif";s:4:"0f7a";s:16:"nested_icon2.gif";s:4:"ea6d";s:7:"tca.php";s:4:"436a";s:14:"doc/manual.sxw";s:4:"1891";s:30:"pi1/class.tx_kbnescefe_pi1.php";s:4:"6d9d";s:17:"pi1/locallang.xml";s:4:"dfeb";s:14:"res/2cols.html";s:4:"a575";s:14:"res/3cols.html";s:4:"d1b3";s:17:"res/be_2cols.html";s:4:"a575";s:17:"res/be_3cols.html";s:4:"d1b3";s:22:"res/be_horiz_cols.html";s:4:"978e";s:19:"res/horiz_cols.html";s:4:"fa62";}',
+	'_md5_values_when_last_written' => 'a:34:{s:9:"ChangeLog";s:4:"6e6f";s:10:"README.txt";s:4:"6572";s:20:"class.ext_update.php";s:4:"2439";s:37:"class.tx_kbnescefe_contentPreview.php";s:4:"3094";s:37:"class.tx_kbnescefe_dbNewContentEl.php";s:4:"566e";s:27:"class.tx_kbnescefe_func.php";s:4:"bfcb";s:33:"class.tx_kbnescefe_impexpHook.php";s:4:"9a04";s:31:"class.tx_kbnescefe_itemproc.php";s:4:"09ac";s:29:"class.tx_kbnescefe_layout.php";s:4:"8003";s:31:"class.tx_kbnescefe_softrefs.php";s:4:"d4b7";s:35:"class.tx_kbnescefe_t3libtcemain.php";s:4:"61fe";s:28:"class.ux_t3lib_clipboard.php";s:4:"11da";s:26:"class.ux_tx_cms_layout.php";s:4:"6717";s:21:"ext_conf_template.txt";s:4:"1a60";s:12:"ext_icon.gif";s:4:"54da";s:17:"ext_localconf.php";s:4:"af36";s:14:"ext_tables.php";s:4:"1d96";s:14:"ext_tables.sql";s:4:"5158";s:32:"icon_tx_kbnescefe_containers.gif";s:4:"401f";s:13:"locallang.xml";s:4:"a0f8";s:16:"locallang_db.xml";s:4:"6649";s:15:"nested_icon.gif";s:4:"0f7a";s:16:"nested_icon2.gif";s:4:"ea6d";s:7:"tca.php";s:4:"436a";s:14:"doc/manual.sxw";s:4:"66ce";s:30:"pi1/class.tx_kbnescefe_pi1.php";s:4:"6d9d";s:17:"pi1/locallang.xml";s:4:"dfeb";s:14:"res/2cols.html";s:4:"a575";s:14:"res/3cols.html";s:4:"d1b3";s:17:"res/be_2cols.html";s:4:"a575";s:17:"res/be_3cols.html";s:4:"d1b3";s:22:"res/be_horiz_cols.html";s:4:"978e";s:20:"res/be_styles_44.css";s:4:"6174";s:19:"res/horiz_cols.html";s:4:"fa62";}',
 	'suggests' => array(
 	),
 );
