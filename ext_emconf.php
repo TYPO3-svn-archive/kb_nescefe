fbe16c9b8824926769176cc3861bd924a105dcc2
kraftb <kraftb@735d13b6-9817-0410-8766-e36946ffe9aa>
2015-11-18 20:51:37 +0000

    [TASK] Stripped down ext_emconf.php
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/kb_nescefe/branches/TYPO3-4.x@90241 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,48 +1,16 @@
 <?php
-
-########################################################################
-# Extension Manager/Repository config file for ext "kb_nescefe".
-#
-# Auto generated 23-07-2010 10:59
-#
-# Manual updates:
-# Only the data in the array - everything else is removed by next
-# writing. "version" and "dependencies" must not be touched!
-########################################################################
-
 $EM_CONF[$_EXTKEY] = array(
 	'title' => 'KB Nested Content Elements',
 	'description' => 'Allows to created nested content elements.',
 	'category' => 'plugin',
-	'shy' => 0,
 	'version' => '1.1.2',
-	'dependencies' => '',
-	'conflicts' => '',
-	'priority' => '',
-	'loadOrder' => '',
-	'module' => '',
 	'state' => 'stable',
-	'uploadfolder' => 0,
-	'createDirs' => '',
-	'modify_tables' => '',
-	'clearcacheonload' => 0,
-	'lockType' => '',
 	'author' => 'Bernhard Kraft',
 	'author_email' => 'kraftb@think-open.at',
-	'author_company' => '',
-	'CGLcompliance' => '',
-	'CGLcompliance_note' => '',
+	'author_company' => 'think-open.at GmbH',
 	'constraints' => array(
 		'depends' => array(
+			'typo3' => '4.5.0-4.7.99'
 		),
-		'conflicts' => array(
-		),
-		'suggests' => array(
-		),
-	),
-	'_md5_values_when_last_written' => 'a:37:{s:9:"ChangeLog";s:4:"fcfb";s:10:"README.txt";s:4:"6572";s:20:"class.ext_update.php";s:4:"2439";s:37:"class.tx_kbnescefe_contentPreview.php";s:4:"5a11";s:37:"class.tx_kbnescefe_dbNewContentEl.php";s:4:"6786";s:27:"class.tx_kbnescefe_func.php";s:4:"bfcb";s:33:"class.tx_kbnescefe_impexpHook.php";s:4:"9a04";s:31:"class.tx_kbnescefe_itemproc.php";s:4:"94d8";s:29:"class.tx_kbnescefe_layout.php";s:4:"b2a5";s:31:"class.tx_kbnescefe_softrefs.php";s:4:"d4b7";s:35:"class.tx_kbnescefe_t3libtcemain.php";s:4:"61fe";s:28:"class.ux_t3lib_clipboard.php";s:4:"11da";s:26:"class.ux_tx_cms_layout.php";s:4:"6717";s:21:"ext_conf_template.txt";s:4:"1a60";s:12:"ext_icon.gif";s:4:"54da";s:17:"ext_localconf.php";s:4:"af36";s:14:"ext_tables.php";s:4:"1d96";s:14:"ext_tables.sql";s:4:"5158";s:32:"icon_tx_kbnescefe_containers.gif";s:4:"401f";s:13:"locallang.xml";s:4:"a0f8";s:16:"locallang_db.xml";s:4:"6649";s:15:"nested_icon.gif";s:4:"0f7a";s:16:"nested_icon2.gif";s:4:"ea6d";s:7:"tca.php";s:4:"436a";s:14:"doc/manual.sxw";s:4:"66ce";s:30:"pi1/class.tx_kbnescefe_pi1.php";s:4:"6d9d";s:17:"pi1/locallang.xml";s:4:"dfeb";s:14:"res/2cols.html";s:4:"a575";s:14:"res/3cols.html";s:4:"d1b3";s:16:"res/be_2cols.css";s:4:"a525";s:17:"res/be_2cols.html";s:4:"e0ba";s:16:"res/be_3cols.css";s:4:"3415";s:17:"res/be_3cols.html";s:4:"2a26";s:21:"res/be_horiz_cols.css";s:4:"3751";s:22:"res/be_horiz_cols.html";s:4:"35da";s:20:"res/be_styles_44.css";s:4:"bc62";s:19:"res/horiz_cols.html";s:4:"fa62";}',
-	'suggests' => array(
 	),
 );
-
-?>
